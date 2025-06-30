c63664dbfe221dad7583003d7dedc4d9c2a2b2c9
Mansi1975 <amruthammansi@gmail.com>
2025-06-30 15:48:53 +0530

    log/sign in local

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -30,7 +30,7 @@ const Signup = () => {
   }
 
  try {
-  const res = await fetch('https://curve-backtest-forge.onrender.com/signup', {
+  const res = await fetch('http://localhost:5000/signup', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
